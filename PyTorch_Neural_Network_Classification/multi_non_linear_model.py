00776c65d9b289abe19dda022622283ab9bed95f
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-01-24 22:28:40 +0500

    Making My First crossEntropyModel

--- PyTorch_Neural_Network_Classification/multi_non_linear_model.py
+++ PyTorch_Neural_Network_Classification/multi_non_linear_model.py
@@ -12,45 +12,19 @@ X_blob, y_blob = make_blobs(
     n_samples=1000,
     n_features=NUM_FEATURES,
     centers=NUM_CLASSES,
-    cente_std=1.5,
+    cluster_std=1.5,
     random_state=RANDOM_SEED,
 )
 
 
 X_blob = torch.from_numpy(X_blob).type(dtype=torch.float32)
-y_blob = torch.from_numpy(X_blob).type(dtype=torch.float32)
+y_blob = torch.from_numpy(y_blob).type(dtype=torch.float32)
 
 
 X_blob_train, y_blob_train, X_blob_test, y_blob_test = train_test_split(
     X_blob, y_blob, test_size=0.2, random_state=RANDOM_SEED
 )
 
-
-plt.figure(figsize=(10,7))
-plt.plot(X_blob[:,0], X_blob[:,1], c=y_blob,class Parrot:
-
- def fly(self):
-   print('Parrot can fly')
-
- def swim(self):
-   print('Parrot can not swim')
-
-class Penguin:
-
- def fly(self):
-   print('Penguin can not fly')
-
- def swim(self):
-   print('Penguin can swim')
-
-# common interface
-def flying_test(bird):
-  bird.fly()
-
-#instantiate objects
-blu = Parrot()
-peggy = Penguin()
-
-# passing the object
-flying_test(blu)
-flying_test(peggy))
+plt.figure(figsize=(10, 7))
+plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
+plt.show()
