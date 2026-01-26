aff91c24c5d11bc4e9ac36b7e7e5e7fc5bb8db14
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-01-26 21:58:35 +0500

    Removing error

--- PyTorch_Neural_Network_Classification/multi_non_linear_model.py
+++ PyTorch_Neural_Network_Classification/multi_non_linear_model.py
@@ -2,6 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
+from torch import nn
 
 NUM_CLASSES = 4
 NUM_FEATURES = 2
@@ -21,11 +22,64 @@ X_blob = torch.from_numpy(X_blob).type(dtype=torch.float32)
 y_blob = torch.from_numpy(y_blob).type(dtype=torch.float32)
 
 
-X_blob_train, y_blob_train, X_blob_test, y_blob_test = train_test_split(
+X_blob_train, X_blob_test, y_blob_train, y_blob_test = train_test_split(
     X_blob, y_blob, test_size=0.2, random_state=RANDOM_SEED
 )
 
 plt.figure(figsize=(10, 7))
 plt.scatter(X_blob[:, 0], X_blob[:, 1], c=y_blob, cmap=plt.cm.RdYlBu)
-plt.show()
+# plt.show()
 ## Making a multi model
+
+device = "cuda" if (torch.cuda.is_available()) else "cpu"
+
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
+class BlobModel(nn.Module):
+    def __init__(self, input_features, output_features, hidden_units=8):
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features),
+        )
+
+    def forward(self, X):
+        return self.linear_layer_stack(X)
+
+
+model_4 = BlobModel(input_features=2, output_features=4, hidden_units=8).to(device)
+
+
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(params=model_4.parameters(), lr=0.1)
+
+
+epochs = 100
+
+for epoch in range(epochs):
+    model_4.train()
+    y_logits = model_4(X_blob_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
+    print(y_blob_train[0], X_blob_train[0])
+    loss = loss_fn(y_blob_train, y_pred)
+    acc = accuracy_fn(y_true=y_blob_train, y_pred=y_pred)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    with torch.inference_mode():
+        test_logits = model_4(X_blob_train).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        test_loss = loss_fn(test_logits, y_blob_train)
+        test_acc = accuracy_fn(y_true=y_blob_train, y_pred=test_pred)
+    if epoch % 100 == 0:
+        print(
+            f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f} | Test loss: {test_loss:2f}"
+        )
