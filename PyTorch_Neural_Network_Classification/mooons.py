7abda26d2dcc0c12ad6f1818b68acb04ade54036
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-01-27 23:27:51 +0500

    Making training loop

--- PyTorch_Neural_Network_Classification/mooons.py
+++ PyTorch_Neural_Network_Classification/mooons.py
@@ -10,10 +10,10 @@ NUM_FEATURE = 1000
 RANDOM_SEED = 42
 X, y = make_moons(n_samples=NUM_FEATURE, noise=0.3, random_state=RANDOM_SEED)
 
-# X = torch.from_numpy(X).type(dtype=torch.float32)
-# y = torch.from_numpy(X).type(dtype=torch.float32)
+X = torch.from_numpy(X).type(dtype=torch.float32)
+y = torch.from_numpy(y).type(dtype=torch.float32)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+X_moon_train, X_moon_test, y_moon_train, y_moon_test = train_test_split(X, y)
 
 
 plt.scatter(X[:, 0], X[:, 1], c=y)
@@ -23,8 +23,11 @@ plt.show()
 class MoonModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer_1 = (nn.Linear(in_features=2, out_features=10),)
-        self.layer_2 = (nn.Linear(in_features=10, out_features=10),)
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.ReLU = nn.ReLU()
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.ReLU = nn.ReLU()
+
         self.layer_3 = nn.Linear(in_features=10, out_features=1)
 
     def forward(self, X: torch.tensor) -> torch.tensor:
@@ -38,3 +41,14 @@ loss_fn = nn.BCEWithLogitsLoss()
 
 optimizer = torch.optim.Adam(params=model_5.parameters(), lr=0.1)
 
+epochs = 200
+
+for epoch in range(epochs):
+    model_5.train()
+    y_logits = model_5(X_moon_train).squeeze()
+    loss = loss_fn(y_logits, y_moon_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if epoch % 20 == 0:
+        print(loss)
