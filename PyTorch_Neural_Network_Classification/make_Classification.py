114180b78d9ee4135e4cf6ce4f9046b48446cd57
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-01-29 22:33:49 +0500

    Making training loop

--- PyTorch_Neural_Network_Classification/make_Classification.py
+++ PyTorch_Neural_Network_Classification/make_Classification.py
@@ -3,7 +3,9 @@ import torch
 from torch import nn
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
+RANDOM_SEED = 42
 N_SAMPLES = 2000
 N_FEATURE = 10
 N_INFROMATIVE = 6
@@ -24,11 +26,31 @@ X, y = make_classification(
 
 
 class Classifcation_model(nn.Module):
-    def __init__(self, n_feature):
+    def __init__(self, n_features):
         super().__init__()
         model = nn.Sequential(
-            nn.Linear(in_features=n_feature, out_features=128),
+            nn.Linear(in_features=n_features, out_features=128),
+            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(in_features=128, out_features=128),
+            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(in_features=128, out_features=4),
         )
-    
+
+    def forward(self, X):
+        return self.model(X)
+
+
+torch.manual_seed(RANDOM_SEED)
+model_6 = Classifcation_model(n_features=N_FEATURE)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model_6.parameters(), lr=0.05)
+
+epochs = 200
+
+
+for epoch in range(epochs):
+    model_6.train()
+    y_logits
