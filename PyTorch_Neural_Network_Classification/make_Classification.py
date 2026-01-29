a19e7ade3fc69395e9c6bf60c2684381cdec6611
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-01-29 22:23:57 +0500

    Making the model

--- PyTorch_Neural_Network_Classification/make_Classification.py
+++ PyTorch_Neural_Network_Classification/make_Classification.py
@@ -1,4 +1,8 @@
 from sklearn.datasets import make_classification
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 N_SAMPLES = 2000
 N_FEATURE = 10
@@ -17,3 +21,14 @@ X, y = make_classification(
     class_sep=CLASS_SEP,
     flip_y=FLIP_Y,
 )
+
+
+class Classifcation_model(nn.Module):
+    def __init__(self, n_feature):
+        super().__init__()
+        model = nn.Sequential(
+            nn.Linear(in_features=n_feature, out_features=128),
+            nn.Linear(in_features=128, out_features=128),
+            nn.Linear(in_features=128, out_features=4),
+        )
+    
